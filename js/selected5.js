6ee1e618efefd6de6c0de6ec5e45c3adc900f9e6
Ashraf59 <ashrafams59b@gmail.com>
2022-08-22 17:15:58 +0600

    Select-V player list completed

--- js/selected5.js
+++ js/selected5.js
@@ -1,61 +1,32 @@
 const playerList = [];
-// products-cart
+// Best Player List
 
-// function displayProduct()
-// {
-//     let totalPrice = 0;
-//     const totalProducts = document.getElementById("total-products");
-//     totalProducts.innerText = cart.length;
+function selectedPlayer(){
+    
+    const orderItem = document.getElementById('orderList');
+    orderItem.innerHTML = "";
 
-//     const cartContainer = document.getElementById("products-cart");
-//     cartContainer.textContent = '';
+    for(let i = 1; i < playerList.length; i++)
+    {
+        const name = playerList[i].playerName;
 
-//     for(let i = 0; i < cart.length; i++)
-//     {
-//         totalPrice += cart[i].pdPrice;
+        const ol = document.createElement("li");
+        ol.innerHTML = `
+        <ol>${name}</ol>
+        `;
+        orderItem.appendChild(ol);
+    }
 
-//         const tr = document.createElement("tr");
-//         tr.innerHTML = `
-//         <th>${i+1}</th>
-//         <td>${cart[i].pdName}</td>
-//         <td>${cart[i].pdPrice}</td>
-//         `;
-//         cartContainer.appendChild(tr);
-//     }
-//     const tr = document.createElement('tr');
-//     tr.innerHTML = `
-//     <th></th>
-//     <td>Total Price</td>
-//     <td>${totalPrice}</td>
-//     `;
-
-//     cartContainer.appendChild(tr);
-// }
+}
 
 function bestPlayer(element) {
-//     const pdName = element.parentNode.parentNode.children[0].innerText;
-//     const pdPrice = element.parentNode.parentNode.children[1].children[0].innerText;
-
-//     const pd = {
-//         pdName: pdName,
-//         pdPrice: parseInt(pdPrice)
-//     }
-
-//     cart.push(pd);
-    
-//     displayProduct();
-
-console.log(element.parentNode.parentNode.children);
-// console.log(element.parentNode.parentNode.children[0].innerText);
-// console.log(element.parentNode.parentNode.children[0].innerText);
-// 
 
 const playerName = element.parentNode.parentNode.children[0].innerText;
-// console.log(playerName);
+
 const playerObj = {
     playerName: playerName
 }
 playerList.push(playerObj);
-console.log(playerList);
-console.log(playerList.length);
+
+selectedPlayer(playerList);
 }
