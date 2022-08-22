442ae004996a9847fb37e8ccd044615108ab1743
Ashraf59 <ashrafams59b@gmail.com>
2022-08-23 02:28:51 +0600

    Total Budget Accounts Completed

--- js/budget.js
+++ js/budget.js
@@ -1,13 +1,90 @@
+
+//Calculate Player Expense
+
 document.getElementById('calculation').addEventListener('click', function(){
+
+    //Per player cost
+
     const perPlayerCost = document.getElementById('playerCost');
     const perPlayerCostString = perPlayerCost.value;
     const previousPlayerCost = parseFloat(perPlayerCostString);
 
-    const numberOfPlayer = document.getElementById('orderList');
-    const numberOfPlayerString = numberOfPlayer.innerText;
-    const selectedPlayer = parseFloat(numberOfPlayerString);
+    //Convert Seleted number player string to number
 
-    const newPlayer = selectedPlayer * previousPlayerCost;
-    numberOfPlayer.innerText = newPlayer;
+    const numberOfPlayer = document.getElementById('orderList');
+    const playerInnerText = numberOfPlayer.innerText;
+    const playerTextSplit = playerInnerText.split(/\n/);
+    const selectedPlayer = playerTextSplit.length;
+
+    //Slected player total expense
+
+    const playerExpenseText = document.getElementById('player-expense-text');
+    const playerExpenseTextString = playerExpenseText.innerText;
+    const previousExpenseText = parseFloat(playerExpenseTextString);
+
+    const playerSalary = selectedPlayer * previousPlayerCost;
+    playerExpenseText.innerText = playerSalary;
 
 })
+
+
+// Calculate total expenses
+
+
+
+document.getElementById('total-calculation').addEventListener('click', function(){
+
+    
+    //Per player cost
+
+    const perPlayerCost = document.getElementById('playerCost');
+    const perPlayerCostString = perPlayerCost.value;
+    const previousPlayerCost = parseFloat(perPlayerCostString);
+
+    //Convert Seleted number player string to number
+
+    const numberOfPlayer = document.getElementById('orderList');
+    const playerInnerText = numberOfPlayer.innerText;
+    const playerTextSplit = playerInnerText.split(/\n/);
+    const selectedPlayer = playerTextSplit.length;
+
+
+
+     //Slected player total expense
+
+     const playerExpenseText = document.getElementById('player-expense-text');
+     const playerExpenseTextString = playerExpenseText.innerText;
+     const previousExpenseText = parseFloat(playerExpenseTextString);
+     const playerSalary = selectedPlayer * previousPlayerCost;
+     playerExpenseText.innerText = playerSalary;
+
+    //Manager Salary
+
+    const managerSalaryField = document.getElementById('manager-salary');
+    const managerSalaryFieldString = managerSalaryField.value;
+    const managerSalary = parseFloat(managerSalaryFieldString);
+
+    
+    
+
+    //Coach Salary
+
+    const coachSalaryField = document.getElementById('coach-salary');
+    const coachSalaryFieldString = coachSalaryField.value;
+    const coachSalary = parseFloat(coachSalaryFieldString);
+
+    //Calculation of total expense
+
+    const totalExpenseText = document.getElementById('total');
+    const totalExpenseFieldString = totalExpenseText.innerText;
+    const totalExpense = parseFloat(totalExpenseFieldString);
+
+    const totalBudget = playerSalary + managerSalary + coachSalary;
+    totalExpenseText.innerText = totalBudget;
+
+   
+
+})
+
+
+
