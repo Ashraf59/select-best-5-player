a0db4ce62d1895756448904b498eb737765d2b77
Ashraf59 <ashrafams59b@gmail.com>
2022-08-23 12:17:16 +0600

    Alert message added for more than 5 players

--- js/selected5.js
+++ js/selected5.js
@@ -6,24 +6,31 @@ function selectedPlayer(){
     const orderItem = document.getElementById('orderList');
     orderItem.innerHTML = "";
 
-    for(let i = 0; i < playerList.length; i++)
-    {
+    for(let i = 0; i < playerList.length; i++){
+
+    if(playerList.length < 6){
+
         const name = playerList[i].playerName;
 
         const ol = document.createElement("li");
         ol.innerHTML = `
         <ol>${name}</ol>
         `;
-        orderItem.appendChild(ol);
+        orderItem.appendChild(ol);  
     }
-
+ 
+    else{
+        alert('You cannot add player more than 5');
+       return;
+    }
+    
 }
-
+        
+}
+    
 function bestPlayer(element) {
 element.disabled = true;
 const playerName = element.parentNode.parentNode.children[0].innerText;
-
-
 
 const playerObj = {
     playerName: playerName
@@ -31,6 +38,5 @@ const playerObj = {
 playerList.push(playerObj);
 
 selectedPlayer(playerList);
-
 
 }
