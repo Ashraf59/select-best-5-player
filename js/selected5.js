b7a4f33f89f0836a09571aa8a676e69f0069dbba
Ashraf59 <ashrafams59b@gmail.com>
2022-08-23 13:25:05 +0600

    Player Selected some issue fixed

--- js/selected5.js
+++ js/selected5.js
@@ -4,30 +4,25 @@ const playerList = [];
 function selectedPlayer(){
     
     const orderItem = document.getElementById('orderList');
-    orderItem.innerHTML = "";
-
-    for(let i = 0; i < playerList.length; i++){
-
+    
     if(playerList.length < 6){
-
+        orderItem.innerHTML = "";
+        for(let i = 0; i < playerList.length; i++){  
         const name = playerList[i].playerName;
-
         const ol = document.createElement("li");
         ol.innerHTML = `
         <ol>${name}</ol>
         `;
         orderItem.appendChild(ol);  
     }
- 
+}
     else{
         alert('You cannot add player more than 5');
+        element.disabled = false;
        return;
-    }
-    
+    }  
 }
-        
-}
-    
+   
 function bestPlayer(element) {
 element.disabled = true;
 const playerName = element.parentNode.parentNode.children[0].innerText;
